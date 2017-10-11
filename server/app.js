571af5700ba96aba10225d08354f748bcf14b2ed
Pavel <voropaev.p@gmail.com>
2017-10-11 20:36:58 +0100

    zipped sources and clean ups

--- server/app.js
+++ server/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 
 app.set('port', (process.env.PORT || 8080));
+app.get('*.js', function(req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/javascript');
+  next();
+});
+app.get('*.css', function(req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/css');
+  next();
+});
 app.use('/', express.static(__dirname + '/static'));
 
 app.listen(app.get('port'), function() {
