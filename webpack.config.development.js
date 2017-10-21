8a2a2e1754900d830e89ad3eca3b26edc7e3c380
Pavel <voropaev.p@gmail.com>
2017-10-21 21:40:05 +0100

    auto open browser

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,22 +1,18 @@
 const path = require('path')
 const config = require('./webpack.config.base')
+var OpenBrowserPlugin = require('open-browser-webpack-plugin')
 
 config.devtool = 'eval-map'
 config.devServer = {
   contentBase: path.join(__dirname, './server/static'),
   compress: true,
   hot: true,
-  proxy: {
-    '/api/v1': {
-      target: 'http://localhost:5000',
-      secure: true
-    }
-  },
   stats: {
     colors: true
   },
   historyApiFallback: true,
-  publicPath: 'http://localhost'
+  publicPath: 'http://localhost',
 }
+config.plugins.push(new OpenBrowserPlugin({url: 'http://localhost:8080'}))
 
 module.exports = config
